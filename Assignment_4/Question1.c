2e56ec72a2765cc453d0ac799ef8ca893c49836c
pratappatil-tech <pratapsanjaypatil@gmail.com>
2025-05-17 23:34:17 +0530

    Update Question1.c

--- Assignment_4/Question1.c
+++ Assignment_4/Question1.c
@@ -1,17 +1,7 @@
 /*
 
 wirte a program which accept a number from user and display its multiplication of factors.
-
-Input: 12
-Output: 144               (1 * 2 * 3 * 4 * 6 )
-
-Input: 13
-Output: 1                (1)
-
-Input: 10
-Output: 10               (1 * 2 * 5)
-
-*/
+/*
 
 #include <stdio.h>
 
